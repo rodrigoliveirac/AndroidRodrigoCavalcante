fc88befc4a0c1c8a77afaa8407d4312fb6b0c136
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-03-04 20:01:58 -0300

    [2.MenuPrincipalScreen] - Create Menu Principal Screen and then setup nav graph

---
+++ app/src/main/java/com/rodcollab/androidrodrigocavalcante/ClientInfoFragment.kt
@@ -0,0 +1,33 @@
+package com.rodcollab.androidrodrigocavalcante
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rodcollab.androidrodrigocavalcante.databinding.FragmentClientInfoBinding
+
+class ClientInfoFragment : Fragment() {
+
+    private var _binding: FragmentClientInfoBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentClientInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/rodcollab/androidrodrigocavalcante/MenuMainFragment.kt
@@ -0,0 +1,38 @@
+package com.rodcollab.androidrodrigocavalcante
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.rodcollab.androidrodrigocavalcante.databinding.FragmentMenuMainBinding
+
+class MenuMainFragment : Fragment() {
+
+    private var _binding: FragmentMenuMainBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMenuMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.cardViewButtonClients.setOnClickListener {
+            findNavController().navigate(R.id.action_menuMain_to_clientInfo)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
