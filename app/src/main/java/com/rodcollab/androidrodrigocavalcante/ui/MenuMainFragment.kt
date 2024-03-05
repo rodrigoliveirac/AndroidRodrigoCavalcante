f311d53cc23dc591a6fc632b254135925d5b7598
Rodrigo Oliveira <rodrigocavalcante.pro@gmail.com>
2024-03-05 15:38:45 -0300

    [3.ClientDataScreen] - navigate from ClientInfo Screen to OrdersHistoryScreen

--- app/src/main/java/com/rodcollab/androidrodrigocavalcante/ui/MenuMainFragment.kt
+++ app/src/main/java/com/rodcollab/androidrodrigocavalcante/ui/MenuMainFragment.kt
@@ -25,6 +25,7 @@ class MenuMainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.cardViewButtonClients.setOnClickListener {
             findNavController().navigate(R.id.action_menuMain_to_clientInfo)
         }
